68a90e1648f607bacdb826d22413d3c9390796bd
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-21 10:21:44 -0400

    add solution

---
+++ searchAndShout/FISSearchDetailTableViewController.h
@@ -0,0 +1,15 @@
+//
+//  FISSearchDetailTableViewController.h
+//  searchAndShout
+//
+//  Created by Chris Gonzales on 9/20/14.
+//  Copyright (c) 2014 Joe Burgess. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface FISSearchDetailTableViewController : UITableViewController
+
+@property (strong, nonatomic) NSString *searchedAttribute;
+@property (strong, nonatomic) NSArray *filteredDanceMoves;
+@end
--- searchAndShout/FISSearchViewController.m
+++ searchAndShout/FISSearchViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "FISSearchViewController.h"
+#import "FISDataStore.h"
+#import "FISDanceMove.h"
+#import "FISSearchDetailTableViewController.h"
 
 @interface FISSearchViewController ()
 @property (strong, nonatomic) IBOutlet UITextField *nameSearch;
@@ -15,18 +18,11 @@
 @property (strong, nonatomic) IBOutlet UITextField *step3Search;
 @property (strong, nonatomic) IBOutlet UITextField *step4Search;
 
+@property (strong, nonatomic) NSDictionary *searchObject;
+
 @end
 
 @implementation FISSearchViewController
-
-- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
-{
-    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
-    if (self) {
-        // Custom initialization
-    }
-    return self;
-}
 
 - (void)viewDidLoad
 {
@@ -40,7 +36,50 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
+- (IBAction)nameSearchButtonTapped:(id)sender {
+    NSArray *values = [self searchAttribute:@"name" forTerm:self.nameSearch.text];
+    self.searchObject = @{@"attribute": @"name", @"data": values};
+    
+    [self performSegueWithIdentifier:@"searchDetail" sender:self];
+}
+
+- (IBAction)step1SearchButtonTapped:(id)sender {
+    NSArray *values = [self searchAttribute:@"step1" forTerm:self.step1Search.text];
+    self.searchObject = @{@"attribute": @"step1", @"data": values};
+    
+    [self performSegueWithIdentifier:@"searchDetail" sender:self];
+}
+
+- (IBAction)step2SearchButtonTapped:(id)sender {
+    NSArray *values = [self searchAttribute:@"step2" forTerm:self.step2Search.text];
+    self.searchObject = @{@"attribute": @"step2", @"data": values};
+    
+    [self performSegueWithIdentifier:@"searchDetail" sender:self];
+}
+
+- (IBAction)step3SearchButtonTapped:(id)sender {
+    NSArray *values = [self searchAttribute:@"step3" forTerm:self.step3Search.text];
+    self.searchObject = @{@"attribute": @"step3", @"data": values};
+    
+    [self performSegueWithIdentifier:@"searchDetail" sender:self];
+}
+
+- (IBAction)step4SearchButtonTapped:(id)sender {
+    NSArray *values = [self searchAttribute:@"step4" forTerm:self.step4Search.text];
+    self.searchObject = @{@"attribute": @"step4", @"data": values};
+    
+    [self performSegueWithIdentifier:@"searchDetail" sender:self];
+}
+
+-(NSArray *)searchAttribute:(NSString *)attribute forTerm:(NSString *)searchTerm
+{
+    FISDataStore *store = [FISDataStore sharedDataStore];
+    NSFetchRequest *request = [[NSFetchRequest alloc] initWithEntityName:@"FISDanceMove"];
+    NSPredicate *searchPredicate = [NSPredicate predicateWithFormat:@"%K CONTAINS[cd] %@", attribute,searchTerm];
+    request.predicate = searchPredicate;
+    return [store.managedObjectContext executeFetchRequest:request error:nil];
+}
+
 #pragma mark - Navigation
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
@@ -48,7 +87,10 @@
 {
     // Get the new view controller using [segue destinationViewController].
     // Pass the selected object to the new view controller.
+    FISSearchDetailTableViewController *nextVC = segue.destinationViewController;
+    
+    nextVC.searchedAttribute = self.searchObject[@"attribute"];
+    nextVC.filteredDanceMoves = self.searchObject[@"data"];
 }
-*/
 
 @end
